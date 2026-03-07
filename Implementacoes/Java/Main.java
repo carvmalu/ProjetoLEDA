2d53a2f87ea428d2cef750837b32f120358051ef
Laís Lira <laisferlira@gmail.com>
2026-03-07 13:25:44 -0300

    padronizacao dos metodos e aumento de testes

--- Implementacoes/Java/Main.java
+++ Implementacoes/Java/Main.java
@@ -2,80 +2,115 @@ import java.util.*;
 import java.io.*;
 
 //https://blog.formacao.dev/manipulacao-de-arquivos-csv-em-java-leitura-e-escrita/
-// Comando na raiz do projeto: java -cp Implementacoes/Java Main input/entrada.txt Resultados/ 10000,30000,50000,100000
+// Comando na raiz do projeto:  java -cp Implementacoes/Java Main input/entrada.txt Resultados/Java/ 10000,30000,50000,100000
 
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        //Aqui seria os dados/formas padronizadas.
+        //Separando dados.
         String inputFile = args[0]; 
         String path = args[1];  
         String outputFile = "JavaResults.csv";
+        int RUNS = 30;
+        
+        //
+        ArrayListF warmupF = new ArrayListF(10);
+        ArrayList<Integer> warmupNative = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) { 
+            warmupF.add(0, i); 
+            warmupNative.add(0, i); }
 
-     
+        //Aquecimento fazendo todos os metodos.
+        warmupF.search(500); warmupNative.contains(500);
+        warmupF.remove(Integer.valueOf(500)); warmupNative.remove(Integer.valueOf(500));
+
         Runner r = new Runner(inputFile, 100000);
         int[] data = r.getData();
         int[] sizes = r.readString(args[2]);
 
         try (PrintWriter w = new PrintWriter(new FileWriter(path + "/" + outputFile))) {
-            w.println("Language,Implementation,Operation,Metric,N,TimeMS/Bytes");
+            w.println("Language,Implementation,Size,Operation,TimeMS,Bytes");
 
             for (int i = 0; i < sizes.length; i++) {
-                //
                 int n = sizes[i]; int[] currentData = new int[n];
                 for (int j = 0; j < n; j++)
                     currentData[j] = data[j];
-              
-                // Inserção.
-                ArrayListF listF = new ArrayListF(10);
-                double tF = r.measureAdd(listF, currentData);
-                w.println("Java,ArrayListFor,Insercao,Tempo," + n + "," + tF);
-        
-                ArrayList<Integer> listNative = new ArrayList<>();
-                double tNative = r.measureAddNative(listNative, currentData);
-                w.println("Java,ArrayListBuildIn,Insercao,Tempo," + n + "," + tNative);
 
+                // Acumuladores.
+                double sumTFAdd = 0, sumTNAdd = 0;
+                long sumMFAdd = 0, sumMNAdd = 0;
 
-                listF = new ArrayListF(10);
-                long tFM = r.measureAddMemory(listF, currentData);
-                w.println("Java,ArrayListFor,Insercao,Memoria," + n + "," + tFM);
+                double sumTFSearch = 0, sumTNSearch = 0;
+                long sumMFSearch = 0, sumMNSearch = 0;
 
-                listNative = new ArrayList<>();
-                long tNativeM = r.measureAddNativeMemory(listNative, currentData);
-                w.println("Java,ArrayListBuildIn,Insercao,Memoria," + n + "," + tNativeM);
+                double sumTFRemIdx = 0, sumTNRemIdx = 0;
+                long sumMFRemIdx = 0, sumMNRemIdx = 0;
 
+                double sumTFRemVal = 0, sumTNRemVal = 0;
+                long sumMFRemVal = 0, sumMNRemVal = 0;
 
-                //Verificando busca.
-                double tFSearch = r.measureSearch(listF,-5);
-                w.println("Java,ArrayListFor,Search,Tempo," + n + "," + tFSearch);
+                for (int run = 0; run < RUNS; run++) {
 
-                double tNativeSearch =  r.measureSearchNative(listNative, -5);
-                w.println("Java,ArrayListBuildIn,Search,Tempo," + n + "," + tNativeSearch);
+                    // Inserção.
+                    ArrayListF listF = new ArrayListF(10);
+                    sumTFAdd += r.measureAdd(listF, currentData);
 
-                long tFSearchM = r.measureSearchMemory(listF, -5);
-                w.println("Java,ArrayListFor,Search,Memoria," + n + "," + tFSearchM);
+                    ArrayList<Integer> listNative = new ArrayList<>();
+                    sumTNAdd += r.measureAddNative(listNative, currentData);
 
-                long tNativeSearchM =  r.measureSearchNativeMemory(listNative, -5);
-                w.println("Java,ArrayListBuildIn,Search,Memoria," + n + "," + tNativeSearchM);
+                    listF = new ArrayListF(10);
+                    sumMFAdd += r.measureAddMemory(listF, currentData);
 
+                    listNative = new ArrayList<>();
+                    sumMNAdd += r.measureAddNativeMemory(listNative, currentData);
 
-                //Remoção.
-                double tFRemove = r.measureRemove(listF,currentData[0]);
-                w.println("Java,ArrayListFor,Remocao,Tempo," + n + "," + tFRemove);
-                listF.add(listF.size(),(currentData[0]));
-                
+                    // Search.
+                    Integer num = Integer.valueOf(7);
+                    sumTFSearch += r.measureSearch(listF, num);
+                    sumTNSearch += r.measureSearchNative(listNative, num);
+                    sumMFSearch += r.measureSearchMemory(listF, num);
+                    sumMNSearch += r.measureSearchNativeMemory(listNative, num);
 
-                double tNativeRemove =  r.measureRemoveNative(listNative, currentData[0]);
-                w.println("Java,ArrayListBuildIn,Remocao,Tempo," + n + "," + tNativeRemove);
-                listNative.add(listNative.size(), currentData[0]);
+                    // RemocaoIndice.
+                    sumTFRemIdx += r.measureRemove(listF, currentData[0]);
+                    listF.add(0, (currentData[n - 1]));
 
-                long tFRemoveM = r.measureRemoveMemory(listF,currentData[0]);
-                w.println("Java,ArrayListFor,Remocao,Memoria," + n + "," + tFRemoveM);
-                listF.add(listF.size(),(currentData[0]));
+                    sumTNRemIdx += r.measureRemoveNative(listNative, currentData[0]);
+                    listNative.add(0, currentData[n - 1]);
 
-                long tNativeRemoveM =  r.measureRemoveNativeMemory(listNative, currentData[0]);
-                w.println("Java,ArrayListBuildIn,Remocao,Memoria," + n + "," + tNativeRemoveM);
-                listNative.add(listNative.size(),(currentData[0]));
+                    sumMFRemIdx += r.measureRemoveMemory(listF, currentData[0]);
+                    listF.add(0, (currentData[n - 1]));
+
+                    sumMNRemIdx += r.measureRemoveNativeMemory(listNative, currentData[0]);
+                    listNative.add(0, (currentData[n - 1]));
+
+                    // RemocaoValor.
+                    num = Integer.valueOf(currentData[n - 1]);
+                    sumTFRemVal += r.measureRemove(listF, num);
+                    listF.add(0, (currentData[n - 1]));
+
+                    sumTNRemVal += r.measureRemoveNative(listNative, num);
+                    listNative.add(0, currentData[n - 1]);
+
+                    sumMFRemVal += r.measureRemoveMemory(listF, num);
+                    listF.add(0, (currentData[n - 1]));
+
+                    sumMNRemVal += r.measureRemoveNativeMemory(listNative, num);
+                    listNative.add(0, (currentData[n - 1]));
+                }
+
+                // Escrevendo médias.
+                w.println("Java,ArrayListFor," + n + ",AdicaoInicial," + (sumTFAdd / RUNS) + "," + (sumMFAdd / RUNS));
+                w.println("Java,ArrayListBuildIn," + n + ",AdicaoInicial," + (sumTNAdd / RUNS) + "," + (sumMNAdd / RUNS));
+
+                w.println("Java,ArrayListFor," + n + ",Search," + (sumTFSearch / RUNS) + "," + (sumMFSearch / RUNS));
+                w.println("Java,ArrayListBuildIn," + n + ",Search," + (sumTNSearch / RUNS) + "," + (sumMNSearch / RUNS));
+
+                w.println("Java,ArrayListFor," + n + ",RemocaoIndice," + (sumTFRemIdx / RUNS) + "," + (sumMFRemIdx / RUNS));
+                w.println("Java,ArrayListBuildIn," + n + ",RemocaoIndice," + (sumTNRemIdx / RUNS) + "," + (sumMNRemIdx / RUNS));
+
+                w.println("Java,ArrayListFor," + n + ",RemocaoValor," + (sumTFRemVal / RUNS) + "," + (sumMFRemVal / RUNS));
+                w.println("Java,ArrayListBuildIn," + n + ",RemocaoValor," + (sumTNRemVal / RUNS) + "," + (sumMNRemVal / RUNS));
             }
         
         } catch (Exception e) {
