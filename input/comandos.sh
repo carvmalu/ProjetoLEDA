30181faa377ce6d08c0b117fcf426b2d8dd15885
Maria Luísa <maria.luisa.costa.carvalho@ccc.ufcg.edu.br>
2026-03-13 15:34:35 -0300

    Update comandos.sh

--- input/comandos.sh
+++ input/comandos.sh
@@ -8,5 +8,5 @@ set -e
 go -C Implementacoes/Go run . input/entrada.txt Resultados/Go/ 10000,30000,50000,100000 10000
 java -cp Implementacoes/Java Main input/entrada.txt Resultados/Java/ 10000,30000,50000,100000 10000
 python Implementacoes/Python/Main.py input/entrada.txt Resultados/Python 10000,30000,50000,100000 10000
-./main_haskell /input entrada.txt 10000 30000 50000 100000 10000
+./main_haskell input entrada.txt 10000 30000 50000 100000 10000
 ./benchmark "10000,30000,50000,100000" 1000000
